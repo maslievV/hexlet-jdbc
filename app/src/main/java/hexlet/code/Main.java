85ea23ea63deb13788f99e185ca352a0d281c1ef
maslievV <maslievvlad8@gmail.com>
2025-02-16 17:24:03 +0300

    update

--- app/src/main/java/hexlet/code/Main.java
+++ app/src/main/java/hexlet/code/Main.java
@@ -5,26 +5,27 @@ import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
-        var conn = DriverManager.getConnection("jdbc:h2:mem:hexlet_test");
 
-        var sql = "CREATE TABLE users (id BIGINT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(255), age INT)";
-        var statement = conn.createStatement();
-        statement.execute(sql);
-        statement.close();
+        try (var conn = DriverManager.getConnection("jdbc:h2:mem:my_database")) {
 
-        var sql2 = "INSERT INTO users (name, age) VALUES ('Vlad', 19)";
-        var statement2 = conn.createStatement();
-        statement2.executeUpdate(sql2);
-        statement2.close();
+            var sql = "CREATE TABLE users (id BIGINT PRIMARY KEY AUTO_INCREMENT, username VARCHAR(255), age INT)";
+            try (var stmt = conn.createStatement()) {
+                stmt.execute(sql);
+            }
 
-        var sql3 = "SELECT * FROM users";
-        var statement3 = conn.createStatement();
-        var resultSet = statement3.executeQuery(sql3);
-        while (resultSet.next()) {
-            System.out.println("Name: " + resultSet.getString("name"));
-            System.out.println("Age: " + resultSet.getInt("age"));
+            var sql2 = "INSERT INTO users (username, age) VALUES ('Vlad', 19), ('Vika', 19)";
+            try (var stmt2 = conn.createStatement()) {
+                stmt2.executeUpdate(sql2);
+            }
+
+            var sql3 = "SELECT * FROM users";
+            try (var stmt3 = conn.createStatement()) {
+                var rs = stmt3.executeQuery(sql3);
+                while (rs.next()) {
+                    System.out.printf("Name: %s, age: %d%n", rs.getString("username"), rs.getInt("age"));
+                }
+            }
         }
-        statement3.close();
-        conn.close();
+
     }
 }
